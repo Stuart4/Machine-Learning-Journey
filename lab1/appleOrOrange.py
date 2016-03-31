732d18ba1d6f216d8ae669e276e5da6c612b45ed
Jacob Stuart <stuart4@purdue.edu>
2016-03-31 12:19:52 -0400

    adds lab1

---
+++ lab1/appleOrOrange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from sklearn import tree
+
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+labels = [0, 0, 1, 1]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+
+print(clf.predict([[135, 1]]))
